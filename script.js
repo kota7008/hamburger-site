73d5f32eedc5c36a15f18ab217c008a09231e5e4
hoshino kota <ramen7008kota@gmail.com>
2022-06-19 10:03:21 +0900

    archive.html / archive-search.htmlの作成

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-// $(".p-hamburger").click(function () {//ボタンがクリックされたら
-// 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-//     $(".p-sidebar").toggleClass('is-active');//ナビゲーションにクラスを付与
-// });
-
-// $(".p-sidebar").on('touchstart', function () {//ナビゲーションのリンクがクリックされたら
-//     $(".p-hamburger").removeClass('active');//ボタンの activeクラスを除去し
-//     $(".p-sidebar").removeClass('is-active');//ナビゲーションのクラスも除去
-// });
-
-// $(".p-hamburger").click(function () {//ボタンがクリックされたら
-// 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-//     $(".p-sidebar__nav-content").toggleClass('slide');//ナビゲーションにpanelactiveクラスを付与
-// });
-
-// $(".p-sidebar__nav-content").click(function () {//ナビゲーションのリンクがクリックされたら
-//     $(".p-hamburger").removeClass('active');//ボタンの activeクラスを除去し
-//     $(".p-sidebar__nav-content").removeClass('slide');//ナビゲーションのpanelactiveクラスも除去
-// });
-
-// $(".p-hamburger").click(function () {//ボタンがクリックされたら
-// 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-//     $(".p-sidebar").toggleClass('is-active');//ナビゲーションにクラスを付与
-// });
-
-// $(".p-sidebar").on('touchstart', function () {//ナビゲーションのリンクがクリックされたら
-//     $(".p-hamburger").removeClass('active');//ボタンの activeクラスを除去し
-//     $(".p-sidebar").removeClass('is-active');//ナビゲーションのクラスも除去
-// });
-
-$(".p-hamburger").click(function () {//ボタンがクリックされたら
-	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $(".p-sidebar, .p-sidebar__nav-content").toggleClass('slide');//ナビゲーションにpanelactiveクラスを付与
+$(".p-hamburger").click(function () {
+	$(this).toggleClass('active');
+    $(".p-sidebar, .p-sidebar__nav-content").toggleClass('slide');
 });
 
-$(".p-sidebar").on('touchstart',function () {//ナビゲーションのリンクがクリックされたら
-  $(".p-hamburger").removeClass('active');//ボタンの activeクラスを除去し
-    $(".p-sidebar, .p-sidebar__nav-content").removeClass('slide');//ナビゲーションのpanelactiveクラスも除去
+$(".p-sidebar").on('touchstart',function () {
+  $(".p-hamburger").removeClass('active');
+    $(".p-sidebar, .p-sidebar__nav-content").removeClass('slide');
 });
